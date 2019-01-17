bf9e25cdbcd8030f57e90aaa38db03dbb45e2bbe
Ksenia Mironova <ksenia.mironova.95@gmail.com>
2019-01-17 10:50:04 +0100

    Change title

--- index.php
+++ index.php
@@ -76,8 +76,8 @@ $items = $db->query($selectQuery);
 
 <html>
   <head>
-    <title>TodoList</title>
-
+    <!--<title>TodoList</title>-->
+    <strong>TodoList</strong>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
