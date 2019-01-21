c0f208dad127f2d9879a854d6f95391c18bd7bd6
Ksenia Mironova <ksenia.mironova.95@gmail.com>
2019-01-21 15:00:29 +0100

    2nd commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 'on');
 
 
 define('BASE_URL', getenv('TODOLIST_BASE_URL') ?: '/');
-define('DB_USER', getenv('TODOLIST_DB_USER') ?: 'todolist'));
+define('DB_USER', getenv('TODOLIST_DB_USER') ?: 'todolist');
 define('DB_PASS', getenv('TODOLIST_DB_PASS'));
 define('DB_NAME', getenv('TODOLIST_DB_NAME') ?: 'todolist');
 define('DB_HOST', getenv('TODOLIST_DB_HOST') ?: '127.0.0.1');
